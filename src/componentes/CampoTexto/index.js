bee11607d146de2b4bf0c71bb6c15ccd9a7b9f3c
JotapeVogas <joao.chaves@soulasalle.com.br>
2026-01-13 15:31:12 -0300

    adiciona funcionalidade de controle de estado e manipulação de eventos nos componentes CampoTexto e Formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,14 +4,38 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` 
 
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
 
 export default CampoTexto
+
+
+// const usuarios = [
+//         {nome: 'ana', sobrenome: 'silva'},
+//         {nome: 'bia', sobrenome: 'santos'},
+//         {nome: 'carlos', sobrenome: 'oliveira'}
+//     ]
+
+// usuarios.map((usuario) => console.log(usuario))
+// const novosUsuarios = usuarios.map((usuario) => {
+//     return {
+//         nomeCompleto: `${usuario.nome} ${usuario.sobrenome}`
+//     }
+// })
+
+// novosUsuarios.map((usuario) =>  )
+
+// console.log(novosUsuarios)
+
+
